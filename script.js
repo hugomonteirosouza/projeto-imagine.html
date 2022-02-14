46cbdcceccae878e2df53965b170a3440390094a
Hugo Monteiro <hugaodebanquete@gmail.com>
2022-02-14 19:26:31 -0300

    consertei o script e estilizei a fonte e a imagem

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const titulo = ducument.querySelector("#nome");
-const botao = document.querySelector(button);
+const titulo = document.querySelector("#nome");
+const botao = document.querySelector("button");
+
+// console.log(botao);
 
 botao.addEventListener("click", () => {
-titulo.innerHTML = "BEM-VINDO A ZAHIR"
+    titulo.innerHTML = "BEM-VINDO Á ZAHIR"
 });
